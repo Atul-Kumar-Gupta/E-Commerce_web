78437262dfcda546e52ebfca281642cf000feff9
Atul Kumar Gupta <atul@mounttalent.com>
2022-01-24 22:11:58 +0530

    add redux

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,26 +3,35 @@ import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
 import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false)
+  // const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false)
+  const dispatch = useDispatch();
+
+  const producList = useSelector(state => state.producList);
+  const { loading, error, products } = producList
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true)
-        const { data } = await axios.get('/api/products');
-        setLoading(false)
-        setProducts(data)
-      } catch (err) {
-        setError(err.message)
-        setLoading(false)
-      }
-    }
-    fetchData();
-  }, [])
+    // const fetchData = async () => {
+    //   try {
+    //     setLoading(true)
+    //     const { data } = await axios.get('/api/products');
+    //     setLoading(false)
+    //     setProducts(data)
+    //   } catch (err) {
+    //     setError(err.message)
+    //     setLoading(false)
+    //   }
+    // }
+    // fetchData();
+    dispatch(listProducts());
+
+
+  }, [dispatch])
 
   return (
     <div>
